d36900d4f134340c22e8735be901526824f963f2
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-04 23:33:52 +0530

    dynamic values in template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = "Welcome to new Blog";
+  const likes = 50
+  // const person = {name: "John", age:25}
+  // boolean and object cant be dynamically inserted in the template
+  const link = 'http://www.google.com';
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <h2>{likes}</h2>
+        {/* this will give error */}
+        {/* <p>{person}</p> */}
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={link}>Google Site</a>
+      </div>
     </div>
   );
 }
