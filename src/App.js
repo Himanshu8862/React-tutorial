6476547d1e52c3983eaedc1c3673166f839a38ab
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-05 16:45:09 +0530

    added routes and links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from './MyComponents/Navbar';
 import Home from './MyComponents/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './MyComponents/Create';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>The Dojo Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                {/* for react, to not request the server for the page, but to simply render the component we have to use Link tags instead of anchor tags*/}
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: 'white',
                     backgroundColor: '#f1356d',
                     borderRadius: '8px'
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
     );
