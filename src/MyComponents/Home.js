34d900e72e831b081dd87a1eee863e45e2d4e427
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-05 12:28:29 +0530

    json server
    npx json-server --watch data/db.json --port 8000

--- src/MyComponents/Home.js
+++ src/MyComponents/Home.js
@@ -12,28 +12,13 @@ const Home = () => {
         setBlogs(newBlogs);
     })
 
-    const [name, setName] = useState("Mario")
-
-    // useEffect runs on every render, like during page load or change of state
-    // do not change the state inside the useEffect as it will end up in an infinite loop,
-    // change of state will re-render the page and during re-render the state will change again
     useEffect(() => {
         console.log("use effect ran");
-        // console.log(blogs)
-        console.log(name);
-    }, [name])
-    // an empty dependency array will only run useEffect only on first render
-    // without it, useEffect will run every render
-    // we can mention the state values in the array to watch, if that value changes then useEffect will be triggered
-
-    // run the project, 1. without dependency array 2. empty dependency array 3. name in dependency array
-
+    }, [])
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-            <p>{name}</p>
-            <button onClick={() => setName("Luigi")}>Change name</button>
         </div>
     );
 }
