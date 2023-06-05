99a98eab5503cad91250740345d136c4cb27a1d7
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-05 15:48:36 +0530

    custom useFetch hook

--- src/MyComponents/Home.js
+++ src/MyComponents/Home.js
@@ -1,39 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../Hooks/useFetch";
+
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(null);
-
-    // we can not make it an async function and use await keyword, for that we have to externalise the function and do it
-    useEffect(() => {
-        setTimeout(() => {
-            fetch("http://localhost:8000/blogs")
-                .then((res) => {
-                    console.log(res)
-                    if(!res.ok){  // error coming back from server
-                        throw Error('could not fetch the data for that resource')
-                    }
-                    return res.json()
-                })
-                .then((data) => {
-                    setBlogs(data)
-                    setIsLoading(false);
-                    setError(null);
-                })
-                .catch((err)=>{
-                    // auto catches network/connection error
-                    setIsLoading(false);
-                    setError(err.message);
-
-                })
-
-        }, 1000);
-    }, [])
-
+    const {data: blogs, isLoading, error} = useFetch("http://localhost:8000/blogs")
     return (
         <div className="home">
-            { error && <div>{ error }</div> }
+            {error && <div>{ error }</div> }
             {isLoading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs" />}
         </div>
