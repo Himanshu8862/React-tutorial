af53a09b65295c22e3620ef9bc281afcac8013e0
Himanshu8862 <himanshukumargupta307@gmail.com>
2023-06-08 18:39:53 +0530

    added new blog form

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -7,11 +7,7 @@ const Navbar = () => {
             <div className="links">
                 {/* for react, to not request the server for the page, but to simply render the component we have to use Link tags instead of anchor tags*/}
                 <Link to="/">Home</Link>
-                <Link to="/create" style={{
-                    color: 'white',
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px'
-                }}>New Blog</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
